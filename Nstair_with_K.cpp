7b3ff2e04925d3da8e1c49517fb5bfc17e3da6cb
Jai Pratap Singh <43641473+jai13598@users.noreply.github.com>
2020-06-08 17:05:20 +0530

    Update Nstair_with_K.cpp

--- Nstair_with_K.cpp
+++ Nstair_with_K.cpp
@@ -5,10 +5,10 @@ int Nstair(int n,int k){
 	if(n==0){
 		return 1;
 	}
-	//recursive case
 	if(n<0){
 		return 0;
 	}
+	//recursive case
 	int sum=0;
 	for(int i=1;i<=k;i++){
 		  sum+=Nstair(n-i,k);
